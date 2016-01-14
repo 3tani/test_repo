2352d183788a9446f842e876fe225f12d9152cfd
3tani <kir1casiss2@yahoo.co.jp>
2016-01-14 16:43:05 +0900

    masterブランチ変更

--- data/library/library_sample_2.php
+++ data/library/library_sample_2.php
@@ -3,5 +3,16 @@
 	$test = 2;
 	$test += 1; 
 	$test *= 2;
-	echo $test;
+	
+	$ary[] = 1;
+	$ary[] = 2;
+	$ary[] = 3;
+	$ary[] = 4;
+	$ary[] = 5;
+	
+	foreach($ary AS $key => $val){
+		$result += $val;
+	}
+	
+	echo $result;
 ?>
